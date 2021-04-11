bae8c5862ca522be0b0faf56b872e646059c43f0
RD <ricardodias169@gmail.com>
2021-04-10 22:08:38 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            double num1,num2;
+            Console.WriteLine("-------------------");
+            Console.WriteLine("--Maior 2 Numeros--");
+            Console.WriteLine("-------------------");
+            Console.Write("Digite o primeiro número...:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número...:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            if(num1 > num2){
+                Console.WriteLine($"o maio número é {num1}");
+            }
+            else if (num1 < num2){
+                 Console.WriteLine($"o maio número é {num2}");
+            }
+
+
+        }
+    }
+}
